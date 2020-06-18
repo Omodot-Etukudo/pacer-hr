e6a87aff9fbf3ea174e2fe07ae15639f0ba87e3b
Omodot_E <omodote@hotmail.com>
2020-06-18 14:05:51 +0100

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.vue",
+
+    // etc.
+  ],
+
+  defaultExtractor: (content) => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+    return broadMatches.concat(innerMatches);
+  },
+});
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
---
+++ src/components/navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="flex bg-white w-full h-20 px-5 py-3 flex justify-between shadow-md fixed">
+    <div class="flex flex-row">
+      <div>
+        <img class="h-10 ml-5 mr-10 my-2" src="@/assets/pacer-hrlogo.png" alt="Pacer-HR" />
+      </div>
+      <div class="flex">
+        <ul class="font-semibold text-base font-sans">
+          <a
+            href="/"
+            class="inline-block mx-10 my-4 hover:text-blue-700 transition transition-colors duration-300"
+          >Home</a>
+          <a
+            href="/"
+            class="inline-block mx-10 my-4 hover:text-blue-700 transition transition-colors duration-300"
+          >How It works</a>
+          <a
+            href="/careers"
+            class="inline-block mx-10 my-4 hover:text-blue-700 transition transition-colors duration-300"
+          >Careers</a>
+        </ul>
+      </div>
+    </div>
+    <div class="flex flex-row items-start">
+      <a
+        class="px-3 py-2 mt-2 text-base font font-semibold font-sans hover:text-blue-700 transition transition-colors duration-300"
+        href="#"
+      >Contact Us</a>
+      <button
+        class="px-6 py-2 mt-2 mr-10 ml-5 bg-blue-900 text-white rounded-md hover:bg-blue-800 hover:shadow-md focus:outline-none font-semibold font-sans"
+      >Sign In</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navbuttons: [
+        { index: "1", link: "Contact Us" },
+        { index: "2", link: "Sign In", linkto: "signin" }
+      ]
+    };
+  }
+};
+</script>
