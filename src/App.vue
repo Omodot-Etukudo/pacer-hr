d318f93908ea8b738c657bf6bcee41619baf8325
Omodot_E <omodote@hotmail.com>
2020-08-10 12:36:03 +0100

    responsive design in progress

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app" class=" overflow-x-hidden">
+    <router-view class="sm: hidden md:hidden lg:grid" />
+    <div class="text-center px-5 py-32">
+      <img src="./assets/home-images/no-view.svg" class="mx-auto w-2/4" />
+      <h1
+        class="lg:hidden md:grid sm:grid mt-2 font-semibold text-2xl text-blue-900"
+      >
+        Opps!
+        <p>Recommended for Large Screens</p>
+      </h1>
+      <p class="lg:hidden md:grid sm:grid text-gray-600">
+        Responsive optimization still in progress
+      </p>
+    </div>
   </div>
 </template>
 
